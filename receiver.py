2a30626924135009067b58946a6e568a7e13d06f
Aidil Rezjki S S <riskisultan@yahoo.com>
2019-09-21 14:21:48 +0700

    Basic packet.

--- receiver.py
+++ receiver.py
@@ -6,6 +6,12 @@ UDP_PORT = 8080
 udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 udp_socket.bind((UDP_IP, UDP_PORT))
 
+def read_packet(packet):
+    result = bytearray(5)
+    for i in range(0, 5):
+        result[i] = packet[7 + i]
+    return result
+
 while True:
     data, addr = udp_socket.recvfrom(1024)
-    print ("Received: ", data)
+    print ("Received: ", read_packet(data))
--- sender.py
+++ sender.py
@@ -4,5 +4,39 @@ UDP_IP = "127.0.0.1"
 UDP_PORT = 8080
 MESSAGE = "Hello World!"
 
+package_types = [
+    0, #DATA
+    1, #ACK
+    2, #FIN
+    3, #FIN-ACK
+]
+
+def generate_packet():
+    id = 0
+    type_id_int = package_types[0] * (2^4) + id
+    sequence_number = bytearray(2)
+    length = bytearray(2)
+    checksum = bytearray(2) 
+
+    data = bytearray(5)
+    data[0] = 72
+    data[1] = 69
+    data[2] = 72
+    data[3] = 69
+
+    packet = bytearray(7 + 5)
+    packet[0] = type_id_int
+    packet[1] = sequence_number[0]
+    packet[2] = sequence_number[1]
+    packet[3] = length[0]
+    packet[4] = length[1]
+    packet[5] = checksum[0]
+    packet[6] = checksum[1]
+    
+    for i in range(0, 5):
+        packet[7+ i] = data[i]
+    
+    return packet
+
 udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-udp_socket.sendto(bytes(MESSAGE, "utf-8"), (UDP_IP, UDP_PORT))
+udp_socket.sendto(generate_packet(), (UDP_IP, UDP_PORT))
