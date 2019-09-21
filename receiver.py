259f1c202e33fb2b24733a075cd68175903e059f
EdwardAJ <13517115@std.stei.itb.ac.id>
2019-09-21 20:35:26 +0700

    Finish send whole file module

--- receiver.py
+++ receiver.py
@@ -1,5 +1,7 @@
 import socket
 import os
+from os import listdir
+from os.path import isfile, join
 from packet import *
 from constants import *
 
@@ -13,11 +15,38 @@ def read_packet_data(packet):
         result[i] = packet[7 + i]
     return result
 
+def read_file (filename):
+    with open (filename, 'rb') as fin:
+        return fin.read()
+
+def get_file_list (id):
+    my_path = os.path.abspath(os.path.dirname(__file__))
+    complete_path = my_path + '/'+ str(id)
+    onlyfiles = [f for f in listdir(complete_path) if isfile(join(complete_path, f))]
+    print(onlyfiles)
+    return onlyfiles
+
+
+def create_merged_file (id):
+    my_path = os.path.abspath(os.path.dirname(__file__))
+    complete_path = my_path + '/'+ str(id)
+    print('complete_path: ', complete_path)
+    complete_name = complete_path + '/merged'
+    print(complete_name)
+    
+    file_list_array = get_file_list(id)
+    write_file(b'\x00', complete_name)
+    file_list_array.sort()
+    print(file_list_array)
+    f = open(complete_name, "wb")
+    for file_name in file_list_array:
+        buf = read_file(complete_path + '/' + file_name)
+        f.write(buf)
+        f.seek(0,2)
 
 def write_file(data, filename):
     with open(filename, "wb") as binary_file:
         # Write text or bytes to the file
-        binary_file.write("Write text by encoding\n".encode('utf8'))
         num_bytes_written = binary_file.write(data)
         print("Wrote %d bytes." % num_bytes_written)
 
@@ -34,12 +63,6 @@ def write_directory (data, id):
         complete_name = os.path.join(complete_path, str(file_name_int))
     write_file(data, complete_name)
 
-while True:
-    data, addr = udp_socket.recvfrom(DATA_MAX_SIZE + 7)
-    # Get packet id
-    packet_read = read_packet_data(data)
-    write_directory(packet_read, 2)
-
 def receiver():
     i = 0
     while True:
@@ -54,4 +77,14 @@ def receiver():
                 #send_packet(create_ack(packet))
         print ("Received: ", i)
 
-receiver()
+
+# while True:
+#     data, addr = udp_socket.recvfrom(DATA_MAX_SIZE + 7)
+#     # Get packet id
+#     packet_read = read_packet_data(data)
+#     write_directory(packet_read, 2)
+create_merged_file(2)
+# receiver()
+# get_file_list(2)
+
+
