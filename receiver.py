20db3480002590b47263a400e1072910d432757f
elvinaang <13517079@std.stei.itb.ac.id>
2019-09-21 15:46:37 +0700

    write file

--- receiver.py
+++ receiver.py
@@ -11,6 +11,14 @@ def read_packet(packet):
         result[i] = packet[7 + i]
     return result
 
+def write_file(data):
+    with open("hasil.txt", "wb") as binary_file:
+        # Write text or bytes to the file
+        binary_file.write("Write text by encoding\n".encode('utf8'))
+        num_bytes_written = binary_file.write(data)
+        print("Wrote %d bytes." % num_bytes_written)
+
 while True:
     data, addr = udp_socket.recvfrom(DATA_MAX_SIZE + 7)
+    write_file(read_packet(data))
     print ("Received: ", read_packet(data))
